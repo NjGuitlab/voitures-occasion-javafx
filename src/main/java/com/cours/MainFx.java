ebf66516fe381740c1a2ecd64a3578ae4a538265
Jean-Francois Pierre <e0429114@cmaisonneuve.qc.ca>
2026-09-10 20:42:03 -0400

    feat: initialisation des éléments du Controller et ajustement du fxml

--- src/main/java/com/cours/MainFx.java
+++ src/main/java/com/cours/MainFx.java
@@ -1,18 +1,26 @@
 package com.cours;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainFx extends Application {
+    public static void main(String[] args) {launch(args);}
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
 
         stage.setTitle("Voitures d'occasion");
+        stage.setScene(scene);
+        stage.setMinWidth(900);
+        stage.setMinHeight(600);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
--- src/main/java/com/cours/controller/MainController.java
+++ src/main/java/com/cours/controller/MainController.java
@@ -1,4 +1,55 @@
 package com.cours.controller;
 
+import com.cours.model.Voiture;
+import com.cours.service.VoitureService;
+import com.cours.util.LectureCSV;
+import com.cours.util.SourceDonnees;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
+
 public class MainController {
+
+    // On implémente une instance de Voiture service dans le Controller
+    private final VoitureService service = new VoitureService(new LectureCSV());
+
+    private List<Voiture> listeVoitures;
+
+    public MainController(){}
+
+    @FXML
+    private TextField champRecherche;
+
+    @FXML
+    private Slider sliderKilometrage, sliderAnnee, sliderPrix;
+
+    @FXML
+    private ComboBox comboCarburant, comboVille, comboTypeTri, comboMarques;
+
+    @FXML
+    private FlowPane cardsContainer;
+
+    @FXML
+    private Button btnPagePrecedenteCards, btnPageSuivanteCards, buttonVoirBenchmark, btnPagePrecedenteFavoris, btnPageSuivanteFavoris;
+
+    @FXML
+    private RadioButton radioTransmissionToutes, radioTransmissionAuto, radioTransmissionManuelle;
+
+
+    @FXML
+    public void initialize(){
+
+        if (service != null) {
+            this.listeVoitures = service.getVoitures();
+        }
+        // Pour mettre toutes les marques distinctes dans le ComboBox des marques
+        comboMarques.setItems(FXCollections.observableArrayList(listeVoitures.stream().map(Voiture::getMarque).distinct().toList()));
+    }
+
+
+
 }
