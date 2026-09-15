fc5d61da45c3cc8f321c352e78b00b4cbf250cf7
Jean-Francois Pierre <e0429114@cmaisonneuve.qc.ca>
2026-09-14 21:38:29 -0400

    feat: premiere version du schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,54 @@
-CREATE DATABASE IF NOT EXISTS voitures_ocassion;
+DROP TABLE IF EXISTS marques CASCADE;
+DROP TABLE IF EXISTS voitures CASCADE;
+DROP TYPE IF EXISTS type_carburant CASCADE;
+DROP TYPE IF EXISTS transmission CASCADE;
+DROP TYPE IF EXISTS type_vendeur CASCADE;
+
+-- Création de la table Marques
+CREATE TABLE marques (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL UNIQUE,
+    date_ajout TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Création des types pour les voitures
+CREATE TYPE type_carburant AS ENUM (
+    'ESSENCE',
+    'DIESEL',
+    'HYBRIDE',
+    'ELECTRIQUE',
+    'HYBRIDE_RECHARGEABLE'
+);
+
+CREATE TYPE transmission AS ENUM (
+    'AUTOMATIQUE',
+    'MANUELLE'
+);
+
+CREATE TYPE type_vendeur AS ENUM (
+    'PARTICULIER',
+    'CONCESSIONNAIRE'
+);
+
+-- Création de la table Voitures
+CREATE TABLE voitures (
+    id SERIAL PRIMARY KEY,
+    id_marque INT NOT NULL,
+    modele VARCHAR(80) NOT NULL,
+    annee INT NOT NULL,
+    kilometrage INT NOT NULL,
+    prix INT NOT NULL,
+    carburant type_carburant NOT NULL,
+    transmission transmission NOT NULL,
+    couleur VARCHAR(80),
+    ville VARCHAR(100) NOT NULL,
+    type_vendeur type_vendeur NOT NULL,
+    date_publication DATE DEFAULT CURRENT_DATE,
+    description TEXT,
+    CONSTRAINT fk_id_marque FOREIGN KEY (id_marque) REFERENCES marques(id),
+    CONSTRAINT ck_annee_valide CHECK ( annee >= 1900 ),
+    CONSTRAINT ck_kilometrage_positif CHECK ( kilometrage >= 0 ),
+    CONSTRAINT ck_prix_positif CHECK ( prix > 0 )
+);
+
+
